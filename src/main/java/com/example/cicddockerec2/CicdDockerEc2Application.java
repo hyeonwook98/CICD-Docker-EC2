8602a02a5ff3c4d40501a655d49696f13915eb0b
hyeonwook98 <hyeonwook98@naver.com>
2023-05-10 17:39:45 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/example/cicddockerec2/CicdDockerEc2Application.java
@@ -0,0 +1,13 @@
+package com.example.cicddockerec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDockerEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDockerEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicddockerec2/CicdDockerEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicddockerec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDockerEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
