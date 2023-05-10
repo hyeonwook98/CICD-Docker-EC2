0b1619d259c6f53a3718551a76d8d5955d948528
hyeonwook98 <hyeonwook98@naver.com>
2023-05-10 20:41:16 +0900

    feat: 코드 수정

---
+++ src/main/java/com/example/cicddockerec2/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.example.cicddockerec2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/")
+    public String index(){
+        return "docker기반 ec2서버에 cicd 적용 예제 프로젝트 입니다.";
+    }
+}
